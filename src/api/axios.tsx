45d7d3d27a32019534b652d9a1cc989215cf3c46
Shivam Kumar <shivamkumar776499@gmail.com>
2025-07-21 15:50:32 +0530

    bug fix

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://doctorappointmentbackend-ilmx.onrender.com/',
+  baseURL: 'https://doctorappointmentbackend-ilmx.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
